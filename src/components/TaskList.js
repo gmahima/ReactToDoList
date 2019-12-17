80597ba6f5639f43d9b892b1ba178f32f7a796ec
Mahima Gangavarapu <39587007+gmahima@users.noreply.github.com>
2019-12-17 21:47:04 +0530

    fix bug

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,21 +1,16 @@
 import React from "react";
-import {Consumer} from './context/index'
+import { Consumer } from "./context/index";
 export default function TaskList() {
-
   return (
     <Consumer>
-      {
-        (context) =>{
-          const taskList = context.tasks.map((task, index) => (
-            <div>
-              <h3>{task.name}</h3>
-            </div>
-            )
-           )
-          return (
-<div>{taskList}</div>
-          )
-        }
-      }
+      {context => {
+        const taskList = context.tasks.map((task, index) => (
+          <div>
+            <h3>{task.name}</h3>
+          </div>
+        ));
+        return <div>{taskList}</div>;
+      }}
     </Consumer>
-  )
+  );
+}
