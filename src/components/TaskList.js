a0acf0bfa1c5f1c7e28f0241ad15182bb09415eb
Mahima Gangavarapu <mahimagangavarapu@Mahimas-MacBook-Air.local>
2020-01-09 23:45:23 +0530

    add radio

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,8 +1,9 @@
-import React, {useContext} from "react";
+import React, {useContext, useState} from "react";
 import Task from "./Task";
 //import { Div } from "./styled";
 import styled from "styled-components";
 import { TaskContext } from "./context/index";
+import { RadioGroup, RadioButton } from 'react-radio-buttons';
 
 const Div = styled.div`
   padding: 2px;
@@ -12,7 +13,23 @@ const Div = styled.div`
 export default function TaskList(props) {
   const context = useContext(TaskContext);
   let tasks;
-  switch(props.tasks) {
+  // switch(props.tasks) {
+  //   case "allTasks": tasks = context.tasks;
+  //   break;
+  //   case "todoTasks": tasks= context.todoTasks;
+  //   break;
+  //   case "doneTasks": tasks=context.doneTasks;
+  //   break;
+  //  default: tasks=[];
+  //   break;
+
+  // }
+ const [selected, setSelected] = useState("allTasks");
+const radioChange = (event) =>{
+  console.log("selected: " + selected)
+  setSelected(event.target.value);
+}
+  switch(selected) {
     case "allTasks": tasks = context.tasks;
     break;
     case "todoTasks": tasks= context.todoTasks;
@@ -30,8 +47,33 @@ export default function TaskList(props) {
     <Task id={task.id} key={task.id.toString()} />
   ));
   return (
+    [
+      <label>
+        <input
+        type='radio'
+        value='allTasks'
+        checked={selected === "allTasks"}
+        onChange={radioChange}
+        />All Tasks
+      </label>,
+      <label>
+        <input
+        type='radio'
+        value='todoTasks'
+        checked={selected === "todoTasks"}
+        onChange={radioChange}
+        />To Do Tasks
+      </label>,
+      <label>
+        <input
+        type='radio'
+        value='doneTasks'
+        checked={selected === "doneTasks"}
+        onChange={radioChange}
+        />Done Tasks
+      </label>,
     <div>
       <Div tasks={tasks}>{taskList}</Div>
-    </div>
+    </div>]
   );
 }
